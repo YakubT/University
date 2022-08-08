58fc2d2a55d39dac6e8ab437b42124310b6caf0e
YakubT <goldyakub@gmail.com>
2022-08-08 23:08:39 +0300

    I created makeReport for Rector

--- src/main/java/com/solvd/laba/Main.java
+++ src/main/java/com/solvd/laba/Main.java
@@ -9,33 +9,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        /*
-        Faculty fam = new Faculty("FAM");
-        ArrayList<EducationalProgram> list1 = new ArrayList<EducationalProgram>();
-        list1.add(new EducationalProgram(121,1,"Software Engineering"));
-        fam.setListOfEducationalProgram(list1);
-        //
-        StudentCard studentCard = new StudentCard();
-        studentCard.setFaculty(fam);
-        studentCard.setIdOfEduProgram(1);
-
-        Student tolik = new Student("Yakubyshyn","Anatolii","Serhiyovich",Gender.MALE,"abcd00991","NTUU KPI",studentCard, EducationalDegree.BACHELOR);
-        tolik.getStudentCard().setStudentCardId("100bc78");
-        tolik.getStudentCard().setFaculty(fam);
-        System.out.println(tolik.toString());
-        //
-        Dean Dychka = new Dean("Dychka","Ivan","Andriyovich",Gender.MALE,"297uuu","NTUU KPI");
-        Dychka.setFaculty(fam);
-        Dychka.setNameOfDepartment("ПЗКС");
-        System.out.println(Dychka.toString());
-        ArrayList<Student> list = new ArrayList<Student>();
-        list.add(tolik);
-        System.out.println(Dychka.makeReport(list));
-        */
-        StudentCard studentCard = new StudentCard();
-        Student tolik = new Student("Yakubyshyn","Anatolii","Serhiyovich",Gender.MALE,"abcd00991","NTUU KPI",studentCard, EducationalDegree.BACHELOR);
-        tolik.getStudentCard().setStudentCardId("100bc78");
-        tolik.getStudentCard().setStartYearOfStudy(2025);
-       // System.out.println(tolik.getCourseOfStudy());
+        Faculty[] faculties = {new Faculty("FAM"),new Faculty("IASA"),new Faculty("FICE")};
+        List<Student> listOfStudent = new ArrayList<Student>();
+        listOfStudent.add(new Student("Yakubyshyn","Anatolii",Gender.MALE,"abcde","KPI",new StudentCard(),EducationalDegree.BACHELOR));
+        listOfStudent.get(0).getStudentCard().setStudentCardId("1");
+        listOfStudent.get(0).getStudentCard().setFaculty(faculties[0]);
+        //faculties[0].setListOfEducationalProgram(new ArrayList<Department>(){new Department("")});
     }
 }
--- src/main/java/com/solvd/laba/university/Rector.java
+++ src/main/java/com/solvd/laba/university/Rector.java
@@ -3,7 +3,7 @@ package com.solvd.laba.university;
 import com.solvd.laba.university.enums.Gender;
 import com.solvd.laba.university.interfaces.MakingReport;
 
-import java.util.List;
+import java.util.*;
 
 public class Rector extends Administration implements MakingReport {
     public Rector() {
@@ -20,6 +20,25 @@ public class Rector extends Administration implements MakingReport {
 
     @Override
     public String makeReport(List<Student> studentList) {
-        return null;
+        HashMap<String,Integer> hashMap = new HashMap<String, Integer>();
+        for (Student student:studentList){
+            Integer cnt = hashMap.get(student.studentCard.getFaculty().toString());
+            int iCnt;
+            if (cnt==null)
+                iCnt = 0;
+            else
+                iCnt = cnt.intValue();
+            iCnt++;
+            hashMap.put(student.studentCard.getFaculty().toString(),iCnt);
+        }
+        String s = "";
+        int cnt=0;
+        Set<Map.Entry<String,Integer>> set = hashMap.entrySet();
+        for (Map.Entry<String,Integer> e:set){
+            s+="There are "+e.getValue().toString()+" at "+e.getKey().toString()+"\n";
+            cnt  += e.getValue().intValue();
+        }
+        s="There are "+String.valueOf(cnt)+" of students at "+getNameOfUniversity()+"\n"+s;
+        return  s;
     }
 }
